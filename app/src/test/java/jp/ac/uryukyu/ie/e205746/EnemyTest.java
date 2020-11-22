b87b636d81ab3c2e50a20ec89abb6b0910147b28
kaishi <e205746@ie.u-ryukyu.ac.jp>
2020-11-22 22:36:14 +0900

    done for report3

--- app/src/test/java/jp/ac/uryukyu/ie/e205746/EnemyTest.java
+++ app/src/test/java/jp/ac/uryukyu/ie/e205746/EnemyTest.java
@@ -14,7 +14,7 @@ class EnemyTest {
         Enemy slime = new Enemy("スライムもどき", 10, 100);
         demoHero.attack(slime);
         slime.attack(demoHero);
-        assertEquals(defaultHeroHp, demoHero.hitPoint);
+        assertEquals(defaultHeroHp, demoHero.getHitPoint());
     }
 }
 
